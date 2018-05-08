c57e7a719944ab15c529e0b1318db59de09db566
studentuser <uceschs@ucl.ac.uk>
2018-05-08 19:44:44 -0400

    Need to use phone to test. There is a bug that can't put ' in putting in questions.

--- uceschs/platforms/browser/www/js/uploadData.js
+++ uceschs/platforms/browser/www/js/uploadData.js
@@ -1,7 +1,11 @@
 
-function startDataUpload() {
-  alert ("start data upload");
+// uploadData.js is the main function js file for the question app
 
+// notify user the data are start to uploaded
+function startDataUpload() {
+  alert ("Start upload");
+
+// to loop through all the radio button value to get the correct answer from user's input
   function getTrueAnswer(){
   if (document.getElementById("check1_true").checked){
       var answer_true = "answer1" ;
@@ -19,7 +23,7 @@ function startDataUpload() {
     }
   var answer_true = getTrueAnswer();
 
-
+  // specify the value from the text user put in
   var question = document.getElementById("question").value;
   var answer1 = document.getElementById("answer1").value;
   var answer2 = document.getElementById("answer2").value;
@@ -27,33 +31,33 @@ function startDataUpload() {
   var answer4 = document.getElementById("answer4").value;
 
 
-  alert(question);
+ // specify post strings
   var postString = "question="+question + "&answer1="+answer1 + "&answer2="+answer2+ "&answer3="+answer3+ "&answer4="+answer4+ "&answer_true="+answer_true;
   
 
-  // now get the geometry values
+  // get the geometry values
   var latitude = document.getElementById("latitude").value;
   var longitude = document.getElementById("longitude").value;
   postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
-  alert("get txt correct");
   processData(postString);
-}
+  }
 
-var client;
-
-function processData(postString) {
-   client = new XMLHttpRequest();
-   client.open('POST','http://developer.cege.ucl.ac.uk:30296/uploadData',true);
-   client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-   client.onreadystatechange = dataUploaded;  
-   client.send(postString);
-}
-// create the code to wait for the response from the data server, and process the response once it is received
-function dataUploaded() {
-  // this function listens out for the server to say that the data is ready - i.e. has state 4
-  if (client.readyState == 4) {
+  // give a variable for processData function
+  var client;
+  // function for posting the data to server
+  function processData(postString) {
+    client = new XMLHttpRequest();
+    client.open('POST','http://developer.cege.ucl.ac.uk:30296/uploadData',true);
+    client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    client.onreadystatechange = dataUploaded;  
+    client.send(postString);
+    }
+    // create the code to wait for the response from the data server, and process the response once it is received
+    function dataUploaded() {
+    // this function listens out for the server to say that the data is ready - i.e. has state 4
+    if (client.readyState == 4) {
     // change the DIV to show the response
     document.getElementById("dataUploadResult").innerHTML = client.responseText;
     }
-}
+  }
 
